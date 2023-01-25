a89a95b2ae815cad58012b423410ecd1226ac42b
FariaAna <anafaria0254@gmail.com>
2023-01-25 21:40:30 +0000

    ainda n é desta.....F

---
+++ DOC/Relatorio.md
@@ -0,0 +1,3 @@
+# Relatório Tcm22-tw-g01 
+
+Trabalho realizado pelo grupo 1 de Ana Faria A041468 e Inês Sousa A041326.
--- SRC/js_files/ALNL.js
+++ SRC/js_files/ALNL.js
@@ -1,64 +1,72 @@
-
 function loadXMLDoc() {
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      console.log(xmlhttp);
-      myFunction(xmlhttp);
-    }
+      if (this.readyState == 4 && this.status == 200) {
+          console.log(xmlhttp);
+          myFunction(xmlhttp);
+      }
   };
-  xmlhttp.open("GET", "./SRC/AL.xml", true);
+  xmlhttp.open("GET", "./SRC/xml_xsd_files/AL.xml", true);
   xmlhttp.send();
 }
 function myFunction(xml) {
   var i;
   var xmlDoc = xml.responseXML;
   console.log(xml);
-  var table = "<tr><th>Nome</th><th>V</th></tr>";
+  var table = "<thead><tr><th>Nome</th><th>V</th><th>D</th><th>%</th></tr></thead>";
+  var tbody = "<tbody>";
   var x = xmlDoc.getElementsByTagName("line");
   for (i = 0; i < x.length; i++) {
-    table += "<tr><td>" +
-      x[i].getElementsByTagName("nome")[0].childNodes[0].nodeValue +
-      "</td>Nome<td>" +
-      x[i].getElementsByTagName("vitorias")[0].childNodes[0].nodeValue +
-      "</td>V</tr>";
-      x[i].getElementsByTagName("derrotas")[0].childNodes[0].nodeValue +
-      "</td>D</tr>";
-    x[i].getElementsByTagName("percentagem")[0].childNodes[0].nodeValue +
-      "</td>%</tr>";
+      tbody += "<tr><td>" +
+          x[i].getElementsByTagName("nome")[0].childNodes[0].nodeValue +
+          "</td><td>" +
+          x[i].getElementsByTagName("vitorias")[0].childNodes[0].nodeValue +
+          "</td><td>" +
+          x[i].getElementsByTagName("derrotas")[0].childNodes[0].nodeValue +
+          "</td><td>" +
+          x[i].getElementsByTagName("percentagem")[0].childNodes[0].nodeValue +
+          "</td></tr>";
   }
+  tbody += "</tbody>";
+  var full = table + tbody;
+  console.log(tbody);
   console.log(table);
-  document.getElementById('demo').innerHTML = table
+  document.getElementById('buttonzau').innerHTML = full
 }
 
 
 function loadXMLDoc() {
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      console.log(xmlhttp);
-      myFunction(xmlhttp);
-    }
+      if (this.readyState == 4 && this.status == 200) {
+          console.log(xmlhttp);
+          myFunction(xmlhttp);
+      }
   };
-  xmlhttp.open("GET", "./SRC/AL.xml", true);
+  xmlhttp.open("GET", "./SRC/xml_xsd_files/NL.xml", true);
   xmlhttp.send();
 }
 function myFunction(xml) {
   var i;
   var xmlDoc = xml.responseXML;
   console.log(xml);
-  var table = "<tr><th>Nome</th><th>V</th></tr>";
+  var table = "<thead><tr><th>Nome</th><th>V</th><th>D</th><th>%</th></tr></thead>";
+  var tbody = "<tbody>";
   var x = xmlDoc.getElementsByTagName("line");
   for (i = 0; i < x.length; i++) {
-    x[i].getElementsByTagName("nome")[0].childNodes[0].nodeValue +
-    "</td>Nome<td>" +
-    x[i].getElementsByTagName("vitorias")[0].childNodes[0].nodeValue +
-    "</td>V</tr>";
-    x[i].getElementsByTagName("derrotas")[0].childNodes[0].nodeValue +
-    "</td>D</tr>";
-  x[i].getElementsByTagName("percentagem")[0].childNodes[0].nodeValue +
-    "</td>%</tr>";
+      tbody += "<tr><td>" +
+          x[i].getElementsByTagName("nome")[0].childNodes[0].nodeValue +
+          "</td><td>" +
+          x[i].getElementsByTagName("vitorias")[0].childNodes[0].nodeValue +
+          "</td><td>" +
+          x[i].getElementsByTagName("derrotas")[0].childNodes[0].nodeValue +
+          "</td><td>" +
+          x[i].getElementsByTagName("percentagem")[0].childNodes[0].nodeValue +
+          "</td></tr>";
   }
+  tbody += "</tbody>";
+  var full = table + tbody;
+  console.log(tbody);
   console.log(table);
-  document.getElementById('demo2').innerHTML = table
+  document.getElementById('demo2').innerHTML = full
 }
